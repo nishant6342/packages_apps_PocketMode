f6d521a351effaf15894c3906b53fe583cde9544
maytinhdibo <iammaytinhdibo@gmail.com>
2022-06-18 09:24:44 +0000

    PocketMode: Import battery friendly pocketmode
    * Squash reference: from https://github.com/maytinhdibo/battery-friendly-pocketmode/commit/a34cb571786302b1ab11728788c9eb62fa806084 to https://github.com/maytinhdibo/battery-friendly-pocketmode/commit/9ceb6c0db0ed269da4a859451eaa88c9754e1ca9
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

---
+++ pocket_mode.mk
@@ -0,0 +1,6 @@
+# Pocket Mode
+PRODUCT_PACKAGES += \
+    PocketMode
+
+PRODUCT_COPY_FILES += \
+    packages/apps/PocketMode/permissions/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pocketmode.xml
