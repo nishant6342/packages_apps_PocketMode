ffe28d05a8914ca8cc8a5be43e9be4d720532410
Nishant Kumar <www.rajsonu13@gmail.com>
2023-02-21 08:29:57 +0530

    PocketMode: Copy permissions to system
    * I missed it being installed to system earlier, and copied permissions to product, causing bootloop if priv-app permissions is enforced

--- pocket_mode.mk
+++ pocket_mode.mk
@@ -3,4 +3,4 @@ PRODUCT_PACKAGES += \
     PocketMode
 
 PRODUCT_COPY_FILES += \
-    packages/apps/PocketMode/permissions/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pocketmode.xml
+    packages/apps/PocketMode/permissions/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pocketmode.xml
